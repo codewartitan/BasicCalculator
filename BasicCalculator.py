381a2aa50f40fac457b2112f556eae87b363f433
codewar <codewartitan@gmail.com>
2019-10-06 14:24:15 +0530

    created addition module

--- BasicCalculator.py
+++ BasicCalculator.py
@@ -3,12 +3,24 @@ import tkinter as tk
 window = tk.Tk()
 window.title('Calculator !')
 window.geometry("400x350")
-Num1 = tk.Label(text="Num1")
-Num1.grid(column=0, row=1)
+Num1 = tk.Label(text="Num1").grid(column=0, row=1)
+Num2 = tk.Label(text="Num2").grid(column=0, row=2)
+Result = tk.Label(text="Result").grid(column=0, row=3)
 Num1Entry = tk.Entry()
+Num2Entry = tk.Entry()
+ResultEntry = tk.Entry()
 Num1Entry.grid(column=1, row=1)
-Num2 = tk.Label(text="Num2")
-Num2.grid(column=0, row=2)
-Num1Entry = tk.Entry()
-Num1Entry.grid(column=1, row=2)
+Num2Entry.grid(column=1, row=2)
+ResultEntry.grid(column=1, row=3)
+
+
+def add():
+    num1 = int(Num1Entry.get())
+    num2 = int(Num2Entry.get())
+    result = num1 + num2
+    ResultEntry.insert(tk.END, str(result))
+
+
+button = tk.Button(window, text="Add", command=add, bg="pink")
+button.grid(column=1, row=5)
 window.mainloop()
